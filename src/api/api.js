170c3f795ace31fc31723b801545596a5c324a9f
Anurag <249453026+phanome@users.noreply.github.com>
2025-12-17 17:10:32 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api", // backend root
+  baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,// backend root
   headers: {
     "Content-Type": "application/json",
   },
